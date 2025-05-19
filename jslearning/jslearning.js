2296ba93cc93e86b160051846a391eb08bed4a23
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-19 14:58:14 +0200

    js learning file

---
+++ jslearning/jslearning.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "John",
+    age: 30,
+    greet: function() {
+        console.log("Hello, my name is " + this.name);
+    }
+}
+
+console.log(person.name);
+
+person.greet();
