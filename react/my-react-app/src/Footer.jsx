bdd7d3f6f5fa9c2d3b6a41489edc1085bc79675c
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-21 09:30:22 +0200

    react basic website - header, content, footer

---
+++ react/my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Moje stránka</p>
+        </footer>
+    );
+}
+
+export default Footer 
