ea113a450ef05a48e5411b507cae0f12a2877952
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-21 09:48:59 +0200

    added cards

---
+++ react/my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return(
+        <div className="card">
+            <img className="card-image" src="https://dopohadky.cz/images/dp/hracky/0/hracky-borek-stavitel.jpg" alt="bořek stavitel mává" />
+            <h2 className="card-title">Bořek</h2>
+            <p className="card-text">Bořek je vedoucí stavební firmy, která v malém městečku a jeho okolí opravuje silnice a domy a provádí ostatní mechanizované práce.</p>
+        </div>
+    );
+}
+
+export default Card
