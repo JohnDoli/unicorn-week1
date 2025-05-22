3c3bac53e608756d4842db3cdaba5aed93e75aff
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-22 14:45:12 +0200

    console log on enter key press

--- toDoList/to-do-list/src/TextInput.jsx
+++ toDoList/to-do-list/src/TextInput.jsx
@@ -1,13 +1,24 @@
+function TextInput({ newItem, setNewItem }) {
 
-
-function TextInput({newItem, setNewItem}) {
+    function keyDown(event) {
+        if (event.key === "Enter") {
+            console.log("Enter key pressed");
+        }
+    }
 
     function handleChange(event) {
         setNewItem(event.target.value);
     }
 
     return (
-        <input className="text-input" type="text" value={newItem} onChange={handleChange} placeholder="new note..." />
+        <input 
+            className="text-input" 
+            type="text" 
+            value={newItem} 
+            onChange={handleChange} 
+            onKeyDown={keyDown} 
+            placeholder="new note..." 
+        />
     );
 }
 
